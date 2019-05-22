8c004b465c1fd340a53f047c42b2cde78a532582
Mutz1982 <thomas.matoe@hotmail.co.nz>
2019-05-22 22:43:55 +1200

    Added Sprite class

--- Pong/src/Pong.java
+++ Pong/src/Pong.java
@@ -4,8 +4,8 @@ import javax.swing.JFrame;
 public class Pong extends JFrame {
 
 	private static final String WINDOW_TITLE = "Pong";
-	private static final int WINDOW_WIDTH = 800;
-	private static final int WINDOW_HEIGHT = 600;
+	private static final int WINDOW_WIDTH = 1000;
+	private static final int WINDOW_HEIGHT = 800;
 	
 	public Pong() {
 		setTitle(WINDOW_TITLE);
