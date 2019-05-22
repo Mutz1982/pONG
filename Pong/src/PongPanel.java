67890348c27cbebe2063942a2c4aae4d44445a96
Mutz1982 <thomas.matoe@hotmail.co.nz>
2019-05-22 20:20:40 +1200

    Added PongPanel with basic constructor to make the screen BLACK

--- Pong/src/PongPanel.java
+++ Pong/src/PongPanel.java
@@ -1,5 +1,41 @@
 import javax.swing.JPanel;
 
-public class PongPanel extends JPanel{
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
+public class PongPanel extends JPanel implements ActionListener, KeyListener{
+	
+	public PongPanel() {
+		setBackground(Color.BLACK);
+	}
+
+	@Override
+	public void keyPressed(KeyEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	
 }
