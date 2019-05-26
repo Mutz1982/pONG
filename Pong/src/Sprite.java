4948208bd4d1716b8a88ef0ffa46d341681403b3
Mutz1982 <thomas.matoe@hotmail.co.nz>
2019-05-26 23:18:16 +1200

    Sprite class complete

--- Pong/src/Sprite.java
+++ Pong/src/Sprite.java
@@ -4,28 +4,60 @@ public class Sprite {
 	private int xPosition, yPosition;
 	private int xVelocity, yVelocity;
 	private int width, height;
-	//private int initialXPosition, initialYPosition;
+	private int initialXPosition, initialYPosition;
 	
-	public int getxPosition() {return xPosition;}
-	public int getyPosition() {return yPosition;}
-	public int getxVelocity() {return xVelocity;}
-	public int getyVelocity() {return yVelocity;}
+	public int getXPosition() {return xPosition;}
+	public int getYPosition() {return yPosition;}
+	public int getXVelocity() {return xVelocity;}
+	public int getYVelocity() {return yVelocity;}
 	public int getWidth() {return width;}
 	public int getHeight() {return height;}
+	public int getInitialXPosition() {return initialXPosition;}
+	public int getInitialYPosition() {return initialYPosition;}
 	
-	public void setxPosition(int newX) {
+	public void setInitialPosition(int initialX, int initialY) {
+		initialXPosition = initialX;
+		initialYPosition = initialY;
+	}
+	
+	
+	public void resetToInitialPosition() {
+		setXPosition(initialXPosition);
+		setYPosition(initialYPosition);
+	}
+	
+	public void setXPosition(int newX) {
 		xPosition = newX;
 	}
-
-	public void setyPosition(int newY) {
+		
+	public void setYPosition(int newY) {
 		yPosition = newY;
 	}
+	
+	public void setXPosition(int newX, int panelWidth) {
+		xPosition = newX;
+		if(xPosition < 0) {
+			xPosition = 0;
+		} else if(xPosition + width > panelWidth) {
+			xPosition = panelWidth - width;
+		}
+		
+	}
 
-	public void setxVelocity(int newXVelocity) {
+	public void setYPosition(int newY, int panelHeight) {
+		yPosition = newY;
+		if(yPosition < 0) {
+			yPosition = 0;
+		} else if(yPosition + height > panelHeight) {
+			yPosition = panelHeight - height;
+		}
+	}
+
+	public void setXVelocity(int newXVelocity) {
 		xVelocity = newXVelocity;
 	}
 
-	public void setyVelocity(int newYVelocity) {
+	public void setYVelocity(int newYVelocity) {
 		yVelocity = newYVelocity;
 	}
 
@@ -35,6 +67,14 @@ public class Sprite {
 
 	public void setHeight(int newHeight) {
 		height = newHeight;
+	}
+
+	public void setInitialXPosition(int initialXPosition) {
+		this.initialXPosition = initialXPosition;
+	}
+
+	public void setInitialYPosition(int initialYPosition) {
+		this.initialYPosition = initialYPosition;
 	}
 	
 }
